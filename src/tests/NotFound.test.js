2e3dfa2bcd2a2b66b246b0f02973fc5ff2c5e5de
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-10-19 16:26:10 -0300

    Implementados testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    const notFoundImg = screen.getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(notFoundTitle).toBeInTheDocument();
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
