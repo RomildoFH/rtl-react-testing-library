f411f759e82715166607d2f06a77964a4cefe4c5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-10-19 16:12:56 -0300

    Implementados teste no componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteMsg = screen.getByText('No favorite pokemon found');
+
+    expect(noFavoriteMsg).toBeInTheDocument();
+  });
+});
